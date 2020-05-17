a9c3523ceef8200a2e03f5aa25b26ab9426e1434
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-05-17 11:06:39 +0200

    Update varPara.lua

--- Day 01/varPara.lua
+++ Day 01/varPara.lua
@@ -1,8 +1,11 @@
-function add(...)  
-  local s = 0  
-    for i, v in ipairs{...} do   --> {...} 表示一个由所有变长参数构成的数组  
-      s = s + v  
-    end  
-    return s  
-  end  
-print(add(3,4,5,6,7))  --->25
+function average(...)
+   result = 0
+   local arg = {...}    --> arg 为一个表，局部变量
+   for i, v in ipairs(arg) do
+      result = result + v
+   end
+   print("总共传入 " .. #arg .. " 个数")
+   return result / #arg
+end
+
+print("平均值为", average(10, 5, 3, 4, 5, 6))
